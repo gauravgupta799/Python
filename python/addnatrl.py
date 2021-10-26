2e2111ab08b6183ea76d3c1c35b34cf31ee25a95
Gaurav Gupta <86770283+gauravgupta799@users.noreply.github.com>
2021-10-27 01:23:27 +0530

    python

---
+++ python/addnatrl.py
@@ -0,0 +1,14 @@
+# Program to add natural
+# numbers up to 
+# sum = 1+2+3+...+n
+
+num =int(input("Enter the number :"))
+sum =0
+i =1
+while i<=num:
+     sum = sum+i
+     i=i+1
+print("The sum is ",sum)
+
+
+
